85f3edc9f0a69583ca937a41da123af3a25f310f
Hannah Stahl <hls62@cornell.edu>
2021-06-26 16:11:10 -0500

    Add file to resource type

--- schemas/resource.js
+++ schemas/resource.js
@@ -9,5 +9,11 @@ export default {
       type: "string",
       validation: Rule => Rule.required(),
     },
+    {
+      title: "File",
+      name: "file",
+      type: "file",
+      validation: Rule => Rule.required(),
+    },
   ],
 };
